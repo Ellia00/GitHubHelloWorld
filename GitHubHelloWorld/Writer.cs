74c1ea8b4bcb6681f1516c5063cb8e4a8cbd481d
Ellia00 <elliotborgkvist@gmail.com>
2017-01-20 10:30:53 +0100

    Skriver Hello World

---
+++ GitHubHelloWorld/Writer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubHelloWorld
+{
+    class Writer
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+
+    }
+}
